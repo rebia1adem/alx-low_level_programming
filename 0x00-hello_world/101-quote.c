5537944e88c0eebfe7889fe40185a5ea48cd38e7
rebia1adem <rebia.adem@gmail.com>
2022-10-29 02:45:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
+ * main - Entry
  * Return: Always 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fwrite(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 			return (1);
-
 }
