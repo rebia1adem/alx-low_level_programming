7be5708ed4dd3786475d228dd77cd060df2ab615
rebia1adem <rebia.adem@gmail.com>
2022-10-30 20:19:37 -0700

    string concatinating

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,19 +1,12 @@
 #include <unistd.h>
 
 /**
- *
- *  * _putchar - writes the caracter
- *
- *   * @c: char to be printed
- *
- *    * Return: always 1 if success
- *
- *     */
-
+ *_putchar - writes the caracter
+ *@c: char to be printed
+ *Return: always 1 if success
+ */
 int _putchar(char c)
 
 {
-
 		return (writes(1, &c, 1));
-
 }
